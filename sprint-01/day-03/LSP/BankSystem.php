cfd38dd871168cc5caf0c3b261c6aedc179048c9
Ibrahim II Musabeh <i.musabah92@gmail.com>
2025-06-18 18:51:42 +0800

    add LSP

---
+++ sprint-01/day-03/LSP/BankSystem.php
@@ -0,0 +1,72 @@
+<?php
+
+interface Account {
+    public function deposit(float $amount): void;
+    public function getBalance(): float;
+}
+ 
+interface withdrawable extends Account{
+    public function withdraw(float $amount): void;
+}
+
+class StandardAccount implements withdrawable {
+    protected float $balance = 0;
+
+    public function __construct() {}
+
+    public function deposit(float $amount): void {
+        $this->balance += $amount;
+        echo "Depositing $amount into Standard Account\n";
+    }
+
+    public function withdraw(float $amount): void {
+        if ($amount > $this->balance) {
+            throw new Exception("Insufficient funds");
+        }
+        $this->balance -= $amount;
+        echo "Withdrawing $amount from Standard Account\n";
+    }
+
+    public function getBalance(): float {
+        return $this->balance;
+    }
+}
+
+class FixedDepositAccount implements Account {
+    protected float $fixedDeposit = 0;
+
+    public function __construct() {}
+
+    public function deposit(float $amount): void {
+        $this->fixedDeposit += $amount;
+        echo "Depositing $amount into Fixed Deposit Account\n";
+    }
+    public function getBalance(): float {
+        return $this->fixedDeposit;
+    }
+}
+
+class BankManagement {
+    public function processWithdrawal(Account $account, float $amount): void {
+        try {
+            $account->withdraw($amount);
+        } catch (Exception $e) {
+            echo "⚠️ " . $e->getMessage() . "\n";
+        }
+    }
+}
+
+$bank = new BankManagement();
+
+$accountTypes = [
+    StandardAccount::class,
+    FixedDepositAccount::class
+];
+
+foreach ($accountTypes as $account) {
+    try {
+        $bank->processWithdrawal(new $account(), 34.2);
+    } catch (Throwable $e) {
+        echo "something went wrong , try again later \n";
+    }
+}
