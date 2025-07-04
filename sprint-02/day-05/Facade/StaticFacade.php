2eff468d5890675b961fc226a16a43be426e0f09
Ibrahim II Musabeh <i.musabah92@gmail.com>
2025-07-05 00:20:12 +0800

    add SPRINT2/5D/FACADE

---
+++ sprint-02/day-05/Facade/StaticFacade.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+abstract class Facade{
+  
+  protected static array $instances = [];
+  abstract protected static function getAccessor():string;
+
+  public static function __callStatic($method,$params){
+
+    $accessor = static::getAccessor();
+
+    if (!isset(static::$instances[$accessor])) {
+        static::$instances[$accessor] = new $accessor;
+    }
+    return static::$instances[$accessor]->$method(...$params);
+    }
+}
+
+class Logger{
+    public function log(string $txt){
+        file_put_contents('app.log', $txt . PHP_EOL, FILE_APPEND);
+    }
+}
+
+class LogFacade extends Facade{
+
+    protected static function getAccessor():string{
+        return Logger::class;
+    }
+}
+
+
+LogFacade::log("hello, Man");
